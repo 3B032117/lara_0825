362acef4ee4eae91d638ad6566b0740081e045b7
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-11-06 16:51:58 +0800

    使用 Model Post 的 delete 以及 destroy 方法刪除資料

--- routes/web.php
+++ routes/web.php
@@ -62,10 +62,19 @@ Route::get('/', function () {
     ]);
 	*/
 	
+	/*
     $post = Post::find(1);
 	$post->title = "saved title";
 	$post->content = "saved content";
 	$post->save();
+	*/
+	
+	$post = Post::find(1);
+	$post->delete();
+	
+	Post::destroy(2);
+	
+	Post::destroy(3, 5, 7);
 	
 	return 'OK!';
 });
