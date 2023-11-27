1156dc69b22ec30101dcb5a5d818f979dc5b2cf9
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2023-11-27 23:28:17 +0800

    使用 Model Post 中的 comments 方法來擷取其所有評論

--- routes/web.php
+++ routes/web.php
@@ -78,6 +78,7 @@ Route::get('/', function () {
 	Post::destroy(3, 5, 7);
 	*/
 	
+	/*
     $allPosts = Post::all();
     dd($allPosts);
 	
@@ -89,6 +90,17 @@ Route::get('/', function () {
 
     $lastPost = Post::orderBy('id','DESC')->first();
     dd($lastPost);
+	*/
+	
+    $post = Post::find(6);
+    echo '標題: '.$post->title.'<br>';
+    echo '內容: '.$post->content.'<br>';
+    echo '--------------------------'.'<br>';
+    $comments = $post->comments()->get();
+    foreach ($comments as $comment){
+        echo '留言: '.$comment->content."<br>";
+        echo '--------------------------'.'<br>';
+    }
 	
 	return 'OK!';
 });
